f54d348f1a413f61716c60e5e15a9cf17b9d0148
Keirra Hall <keirrahall@serenity.gateway.pace.com>
2015-09-02 13:28:43 -0700

    add routes

---
+++ server.py
@@ -0,0 +1,63 @@
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, redirect, request, flash, session as browser_session
+
+app = Flask(__name__)
+app.secret_key = "abc123"
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route("/")
+def index():
+	pass
+
+@app.route("/about")
+def about():
+	pass
+
+@app.route("/fees")
+def fees():
+	pass
+
+@app.route("/contact")
+def contact():
+	pass
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+
+	app.debug = False
+
+
+
+	app.run()
